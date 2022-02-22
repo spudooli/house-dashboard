77f75adb01a9645b722c325fd7551ad8a4e27fb9
Dave <dave@spudooli.com>
2022-02-22 18:07:46 +1300

    Enhance, enhance the deployer

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cp -p /home/dave/Sites/house-dashboard/app.py /var/www/dashboard.spudooli.com/
 echo "Deploying the static assets..."
 
 cp -p /home/dave/Sites/house-dashboard/static/* /var/www/dashboard.spudooli.com/static/
+cp -p /home/dave/Sites/house-dashboard/config.py /var/www/dashboard.spudooli.com/config.py
 
 echo "Deploying the templates..."
 cp -p /home/dave/Sites/house-dashboard/templates/* /var/www/dashboard.spudooli.com/templates/*
